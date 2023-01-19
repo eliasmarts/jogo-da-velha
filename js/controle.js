5fea05e1d17ef255001cfad7dcdabe349cf3b905
Elias Santos Martins <e247057@dac.unicamp.br>
2023-01-19 17:15:01 -0300

    renicio e empate

--- js/controle.js
+++ js/controle.js
@@ -4,16 +4,19 @@ class JogoDaVelha {
     constructor(game) {
         this.game = game;
         this.board = [];
+
+        this.state = 'playing';
+
         for (let i = 0; i < 9; i++)
             this.board[i] = '-';
 
         this.actualPlayer = 'x';
 
-
-        this.circleIcon = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+        this.circleIcon = `<svg style="width:90px;height:90px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
     </svg>`
-        this.xIcon = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+
+        this.xIcon = `<svg style="width:90px;height:90px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
     </svg>`
     }
@@ -23,7 +26,6 @@ class JogoDaVelha {
         for (let i = 0; i < 9; i++) {
             let el = `<div class="campo-jogo" id="campo_${i}"
             onclick="gameControl.play(${i})">
-            oi
             </div>`
 
             game.innerHTML += el;
@@ -32,13 +34,58 @@ class JogoDaVelha {
 
 
     play(position) {
-        this.board[position] = this.actualPlayer;
-        let campo = document.getElementById('campo_' + position);
+        if (this.board[position] == '-' && this.state == 'playing') {
+            this.board[position] = this.actualPlayer;
+            let campo = document.getElementById('campo_' + position);
 
-        if (this.actualPlayer == 'x')
-            campo.innerHTML = this.xIcon;
-        else
-            campo.innerHTML = this.circleIcon;
+            if (this.actualPlayer == 'x')
+                campo.innerHTML = this.xIcon;
+            else
+                campo.innerHTML = this.circleIcon;
+
+            this.switchPlayer();
+
+            this.checkState();
+        }
+    }
+
+
+    switchPlayer() {
+        if (this.actualPlayer == 'x') {
+            this.actualPlayer = 'o'
+        }
+        else {
+            this.actualPlayer = 'x';
+        }
+    }
+
+
+    checkState() {
+        if (this.board.lastIndexOf('-') == -1) {
+            this.updateState('draw');
+        }
+    }
+
+
+    updateState(state) {
+        this.state = state;
+
+        let status = document.getElementById('statusJogo');
+
+        if (state == 'draw')
+            status.innerHTML = 'Empate!';
+        else if (state == 'playing')
+            status.innerHTML = '';
+    }
+
+    restart() {
+        for (let i = 0; i < 9; i++) {
+            this.board[i] = '-';
+            document.getElementById('campo_' + i).innerHTML = "";
+        }
+
+        this.actualPlayer = 'x';
+        this.updateState('playing');
     }
 }
 
